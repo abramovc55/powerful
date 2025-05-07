fb0a6bc67931de7ac9dec06732ef75d674db4109
abramovc55 <abramovickafulkin@gmail.com>
2025-05-07 18:59:06 +0300

    power.lua

--- power.lua
+++ power.lua
@@ -1,2 +1,1 @@
-local powerful = game.Players.LocalPlayer.Kick
-powerful(game.Players.LocalPlayer, "вы были забанены за использование стороннего ПО")
+game.Players.LocalPlayer:Kick("вы были забанены за использование стороннего ПО")
